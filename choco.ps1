9d8747c7daa08618c25da415998b029d98e82f50
Craig Boucher <craig@craigboucher.com>
2018-08-16 23:11:25 -0500

    Create choco.ps1

---
+++ choco.ps1
@@ -0,0 +1,16 @@
+install-packageprovider chocolateyget
+import-packageprovider chocolateyget
+install-package chocolatey -ProviderName chocolateyget
+choco install git -y
+choco install vscode -y
+choco install nodejs -y
+choco install 7zip -y
+choco install azure-cli -y
+choco install nuget.commandline -y
+choco install nugetpackageexplorer -y
+choco install neovim -y
+choco install vscode-editorconfig -y
+choco install vsvim -y
+choco install linqpad -y
+choco install ilspy -y
+choco install msbuild-structured-log-viewer -y
